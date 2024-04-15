44aeb6d3c19ce5e405bd3da221098d9375faba6e
Victor Vieira <victor.h.souza.vieira@gmail.com>
2024-04-15 15:35:29 -0300

    Create middleware files to handle problems

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const notFound = require('./middlewares/notFound');
+const errorHandler = require('./middlewares/errorHandler');
 const cors = require("cors");
 
 app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 app.use('/', routes);
 
 module.exports = app;
